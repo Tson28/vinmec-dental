1eeb17f28b311bfb21d5235d154552f616e7332e
Nguyen Thai Son <son28062005@gmail.com>
2026-05-24 00:50:58 +0700

    fix: change hardcoded url to live render1

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -5,6 +5,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5174',
   'http://127.0.0.1:5173',
+  'https://vinmec-dental.vercel.app',
   'https://vinmec-backend.onrender.com',
 ];
 
